067713c6612dbd56b83e1e53a8f1e683173d99b8
chrish88 <crehall88@gmail.com>
2018-11-04 16:54:10 -0800

    added joi to project, added get, post, put, and validate methods

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     {id: 1, name: 'courses1'},
@@ -15,15 +18,59 @@ app.get('/api/courses', (req, res) =>{
     res.send(courses);
 })
 
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if(error){                                       // if invalid, return 400 - bad request
+        res.status(400).send(error.details[0].message);
+    return;
+    }
+  
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    const course =  courses.find(c => c.id === parseInt(req.params.id));                //look up the course
+    if(!course) res.status(404).send('The course with the given id was not found');     //if not existing, return 404
+
+    const { error } = validateCourse(req.body);
+    if(error){                                       // if invalid, return 400 - bad request
+        res.status(400).send(error.details[0].message);
+    return;
+    }
+
+    course.name = req.body.name;    //update course
+    res.send(course);               //return the updated course
+});
+
+function validateCourse(course){        //function that checks to see if given course is in the database
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);          
+}
+
+
+
+
+
+
+
+
+
+console.log(courses.length)
 app.get('/api/courses/:id', (req, res) =>{
+    console.log(req.params)
    const course =  courses.find(c => c.id === parseInt(req.params.id));
    if(!course) res.status(404).send('The course with the given id was not found');
    res.send(course);
-
-})
+});
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>{
-    console.log(`listening to you on port ${port}`)
-});
+app.listen(port, () =>{console.log(`listening to you on port ${port}`)});
